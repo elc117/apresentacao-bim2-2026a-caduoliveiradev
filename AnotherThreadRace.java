0da7ac112bd372f147c34d95595b824118474ec0
Carlos Eduardo Silva de Oliveira <carloseduardosil.dev@gmail.com>
2026-06-01 12:57:09 -0300

    Codigo ex1

---
+++ AnotherThreadRace.java
@@ -0,0 +1,95 @@
+class Rabbit extends Thread {
+	private String name;
+
+	public Rabbit(String name) {
+		this.name = name;
+	}
+
+	private void runLikeRabbit() {
+		System.out.println(name + " is running fast");
+	}
+
+	public void run() {
+		System.out.println(name + " rabbit is at the start of the race!");
+		for (int pos = 10; pos > 0; pos--) {
+			runLikeRabbit();
+			System.out.println(name + " is at position " + pos);
+			
+			// Dynamic delay for the GIF animation
+			try {
+				Thread.sleep(200); 
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+		System.out.println(name + " rabbit finished the race!");
+	}
+}
+
+class Horse extends Thread {
+	private String name;
+
+	public Horse(String name) {
+		this.name = name;
+	}
+
+	private void runLikeHorse() {
+		System.out.println(name + " is galloping very fast");
+	}
+
+	public void run() {
+		System.out.println(name + " horse is at the start of the race!");
+		for (int pos = 10; pos > 0; pos--) {
+			runLikeHorse();
+			System.out.println(name + " is at position " + pos);
+			
+			// Dynamic delay for the GIF animation
+			try {
+				Thread.sleep(200); 
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+		System.out.println(name + " horse finished the race!");
+	}
+}
+
+class Turtle implements Runnable {
+	private String name;
+
+	public Turtle(String name) {
+		this.name = name;
+	}
+
+	private void runLikeTurtle() {
+		System.out.println(name + " is running slow");
+	}
+
+	public void run() {
+		System.out.println(name + " turtle is at the start of the race!");
+		for (int pos = 10; pos > 0; pos--) {
+			runLikeTurtle();
+			System.out.println(name + " is at position " + pos);
+			
+			// Dynamic delay for the GIF animation
+			try {
+				Thread.sleep(200); 
+			} catch (InterruptedException e) {
+				Thread.currentThread().interrupt();
+			}
+		}
+		System.out.println(name + " turtle finished the race!");
+	}
+}
+
+class AnotherThreadRace {
+	public static void main(String[] args) {
+		Rabbit r = new Rabbit("Snowball");
+		Thread t = new Thread(new Turtle("Donatello"));
+		Horse h = new Horse("Lightning");
+		
+		r.start();
+		t.start();
+		h.start();
+	}
+}
